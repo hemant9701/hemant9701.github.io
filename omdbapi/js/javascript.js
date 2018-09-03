58c5015f8057a249b1e872cd3f337485575e5255
hemant9701 <hemant9701@gmail.com>
2018-09-03 18:29:25 +0530

    omdb

---
+++ omdbapi/js/javascript.js
@@ -0,0 +1,55 @@
+let i; 
+let t;
+let y;
+
+$(document).ready(() => {
+	$('#search-by-title-button').click(function(){
+		i = $("#i").val();
+		t = $("#t").val();
+		y = $("#y").val();
+		if(t=='' & y=='' & i==''){
+			alert("Please enter the value.");
+		}else{
+			fetchData();
+		}
+	});
+	
+});
+
+let fetchData = () => {
+	
+	// API call to get user details
+	
+	$.ajax({
+        type: 'GET',
+        dataType: 'json',
+        async: true,
+        url: 'http://www.omdbapi.com/?apikey=e84540e8&i=' + i + '&y=' + y + '&t=' + t,
+
+        success: (response) => {
+			
+			$('#search-by-title-request').css('display', 'block');
+
+            console.log(response);
+			
+			let dataShow;
+			
+			dataShow = ('<h2>' + response.Title + ' <small>(' + response.Year + ')</small></h2>')
+			dataShow += ('<h4>Director : ' + response.Director + '</h4>')
+			dataShow += ('<h6>Actors : ' + response.Actors + '</h6>')
+			$('#dataShow').html(dataShow);
+			
+			if(response.Poster == null || response.Poster=='N/A'){
+				$('#poster').html('<img src="images/blank_poster.jpg"  id="poster" class="card-img-top"/>');
+			}else{
+				$('#poster').html('<img src="' + response.Poster + '"  id="poster" class="card-img-top"/>');
+			}
+
+        }, error: (err) => {
+
+            console.log(err.responseJSON.error.message);
+            alert(err.responseJSON.error.message)
+
+        }
+	});
+}
