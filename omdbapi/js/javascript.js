f3edcd4691588b2896b770893e83751b77a1e954
hemant9701 <hemant9701@gmail.com>
2018-09-04 17:27:02 +0530

    omdb

--- omdbapi/js/javascript.js
+++ omdbapi/js/javascript.js
@@ -44,9 +44,13 @@ let fetchData = () => {
 				}
 				
 				dataShow += ('<div class="card-body" id="dataShow">');
+				dataShow += ('<p>Type : ' + response.Type + '</p>');
 				dataShow += ('<h2>' + response.Title + ' <small>(' + response.Year + ')</small></h2>');
 				dataShow += ('<h4>Director : ' + response.Director + '</h4>');
 				dataShow += ('<h6>Actors : ' + response.Actors + '</h6>');
+				dataShow += ('<h6><em>Genre : ' + response.Genre + '</em></h6>');
+				dataShow += ('<p><strong>Rating : ' + response.imdbRating + '</strong></p>');
+				dataShow += ('<em>Language : ' + response.Language + '</em>');
 				dataShow += ('<p>Short story : ' + response.Plot + '</p>');
 				dataShow += ('</div>');
 				
